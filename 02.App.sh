6cb00fc83ee4299f75b48eb7af672bdc21aadc96
Sunil <sunil.nunubilli@gmail.com>
2024-08-30 22:23:22 +0530

    Created

---
+++ 02.App.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+LOGDIRECTORY="/var/log/shellogs/"
+FILE=$( echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d:%H:%M:%s)
+LOGFILE="$LOGDIRECTORY/$FILE_$TIMESTAMP"
+
+USER=$(id -u)
+R="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+CHECK_ROOT()
+{
+    if [ $USER -ne 0 ]
+    then
+     echo  -e "you cant install as $R you dnt have req priviliges $N" 
+     exit 1
+     fi
+}
+VALIDATE()
+{
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "$R $2 is SUCCESS $N"
+    else 
+        echo -e "$Y $2 is not FAILED $N"
+    fi
+}
+CHECK_ROOT
+
+dnf module disable nodejs -y &>>LOGFILE
+VALIDATE $? "Disabling nodejs"
