20a692a2194c8f8bc7feeb9b77ce09d19064d987
Sunil <sunil.nunubilli@gmail.com>
2024-09-01 21:36:11 +0530

    Created

--- 02.App.sh
+++ 02.App.sh
@@ -60,7 +60,7 @@ VALIDATE $? "getting the app code"
 cd /app
 rm -rf /app/*
 VALIDATE $? "clearing the app directory"
-unzip /tmp/backend.zip
+unzip /tmp/backend.zip&>>$LOGFILE
 VALIDATE $? "unzipping the code in app directory"
 npm install&>>$LOGFILE
 VALIDATE $? "installing npm in app directory"
